0656c212609a341365a07dc571b73c55fb75b357
phatngo <ntp18799@gmail.com>
2021-12-30 21:11:42 +0700

    Reduce warning in map class

--- Super-Mario-DX10/Map.cpp
+++ Super-Mario-DX10/Map.cpp
@@ -41,7 +41,7 @@ void CMap::Render()
 		{
 			int index = TileMap[CurrentRow][CurrentColumn] - 1;
 			if (index < TotalTiles)
-				Tiles.at(index)->Draw(CurrentColumn * TILE_WIDTH,(CurrentRow * TILE_HEIGHT));
+				Tiles.at(index)->Draw(float(CurrentColumn * TILE_WIDTH),float(CurrentRow * TILE_HEIGHT));
 		}
 }
 void CMap::SetTileMapData(int** TileMapData)
